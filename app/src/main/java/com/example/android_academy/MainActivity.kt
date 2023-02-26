b80fd60c898cb877d16800e32c678477945858ae
mj52951 <mj52951@fer.hr>
2023-02-26 21:35:25 +0100

    Added basic functionality

--- app/src/main/java/com/example/android_academy/MainActivity.kt
+++ app/src/main/java/com/example/android_academy/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.android_academy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn : Button = findViewById(R.id.button)
+        val helloText : TextView = findViewById(R.id.HelloText)
+
+        btn.setOnClickListener{
+            if (btn.text == "HIDE TEXT"){
+                helloText.text = ""
+                btn.text = "SHOW TEXT"
+            } else if (btn.text == "SHOW TEXT"){
+                helloText.text = "Hello World!"
+                btn.text = "HIDE TEXT"
+            }
+        }
     }
 }
