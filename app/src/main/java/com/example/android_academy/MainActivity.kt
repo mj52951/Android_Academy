52ebaa489d213536e62491799fbdbf507b0a77e5
mj52951 <mj52951@fer.hr>
2023-03-05 12:46:03 +0100

    Added FrameLayout and BottomNavigationView to activity_main

--- app/src/main/java/com/example/android_academy/MainActivity.kt
+++ app/src/main/java/com/example/android_academy/MainActivity.kt
@@ -2,8 +2,7 @@ package com.example.android_academy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,17 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn : Button = findViewById(R.id.button)
-        val helloText : TextView = findViewById(R.id.HelloText)
 
-        btn.setOnClickListener{
-            if (btn.text == "HIDE TEXT"){
-                helloText.text = ""
-                btn.text = "SHOW TEXT"
-            } else if (btn.text == "SHOW TEXT"){
-                helloText.text = "Hello World!"
-                btn.text = "HIDE TEXT"
-            }
-        }
     }
 }
