8644332243cbb7a838e4fa98b0c264eb8a31af2f
mj52951 <mj52951@fer.hr>
2023-03-08 15:51:49 +0100

    Added data class Contact

---
+++ app/src/main/java/com/example/android_academy/Contact.kt
@@ -0,0 +1,8 @@
+package com.example.android_academy
+
+data class Contact (val firstName: String, val lastName: String, val age: Int, val oib: Int){
+
+    override fun toString(): String{
+        return "$firstName $lastName $age $oib"
+    }
+}
