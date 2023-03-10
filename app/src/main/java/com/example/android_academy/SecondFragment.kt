c3844781dd93808a1d212508a291ec913e608156
mj52951 <mj52951@fer.hr>
2023-03-10 20:23:25 +0100

    Changed LinearLayout to ConstraintLayout

--- app/src/main/java/com/example/android_academy/SecondFragment.kt
+++ app/src/main/java/com/example/android_academy/SecondFragment.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 
@@ -24,7 +24,7 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val linearLayout: LinearLayout = view.findViewById(R.id.linearLayout01)
+        val constraintLayout: ConstraintLayout = view.findViewById(R.id.constraintLayout03)
 
         viewModel.movies.observe(viewLifecycleOwner) { list ->
 
@@ -32,7 +32,8 @@ class SecondFragment : Fragment() {
                 val tempTextView = TextView(requireContext())
                 tempTextView.text = it.toString()
                 tempTextView.textSize = 25f
-                linearLayout.addView(tempTextView)
+                tempTextView.layout
+                constraintLayout.addView(tempTextView)
             }
         }
     }
